603513ce4dff77c434a41ff0df270a4ee49c3e86
Donatien Dhombres <donatien.dbr@gmail.com>
2021-09-02 16:46:47 +0200

    Added connection to mongoDB

--- server/server.js
+++ server/server.js
@@ -3,7 +3,10 @@ const app = express();
 const { graphqlHTTP } = require('express-graphql');
 const bodyParser = require('body-parser');
 const { buildSchema } = require('graphql');
-const expressPlayground = require('graphql-playground-middleware-express').default
+const expressPlayground = require('graphql-playground-middleware-express').default;
+const mongoose = require('mongoose');
+const config = require('config')
+const db = config.get('mongoURI');
 
 
 app.use(bodyParser.json());
@@ -34,6 +37,19 @@ app.use(
     graphiql: true
   })
 )
+
+//Connect Database
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.log(err.message);
+    //Exist process with failure
+    process.exit(1);
+  }
+}
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
